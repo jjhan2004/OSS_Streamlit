6c8d9d2f5d83bb64aae9419fe3a7ff6762579ff2
jjhan2004 <48384096+jjhan2004@users.noreply.github.com>
2023-11-15 11:03:39 +0900

    Add files via upload

---
+++ streamlit_oss.py
@@ -0,0 +1,9 @@
+import streamlit as st
+OSS_Score = [80, 70, 55, 30, 3, 3, 1, 0]
+
+st.write("# OSS 중간 시험 결과")
+st.write("## OSS 중간 시험 결과")
+st.write("### OSS 중간 시험 결과")
+OSS_Score
+
+st.bar_chart(OSS_Score)
